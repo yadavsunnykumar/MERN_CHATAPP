019483c22e7abf6a16294a28f2feb92073c902da
yadavsunnykumar <sunny9811273802@gmail.com>
2025-04-04 19:32:33 +0530

    This is change in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,11 +27,11 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   // Serve frontend static files
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // Handle any route by serving index.html
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
