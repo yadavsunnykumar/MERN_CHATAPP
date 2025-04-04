e45cf752a788d0496b42b6e11dbf50947eac3ee6
yadavsunnykumar <sunny9811273802@gmail.com>
2025-04-04 19:11:33 +0530

    Fix Vite deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,9 +26,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "dist"))); // Serve static files
+
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 }
 
